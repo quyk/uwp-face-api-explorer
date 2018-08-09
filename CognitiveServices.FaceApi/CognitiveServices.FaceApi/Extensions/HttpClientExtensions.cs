661e0003dcb212a1591aed706916d813dbfd3775
Quyk Mendonça <osilmar@outlook.com>
2018-08-09 13:41:41 -0300

    Adiciona extensão do HttpClient para o metódo Patch

---
+++ CognitiveServices.FaceApi/CognitiveServices.FaceApi/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CognitiveServices.FaceApi.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        private const string Patch = "PATCH";
+
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, HttpContent content)
+        {
+            var method = new HttpMethod(Patch);
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = content
+            };
+
+            return await client.SendAsync(request);
+        }
+
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent content)
+        {
+            var method = new HttpMethod(Patch);
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = content
+            };
+
+            return await client.SendAsync(request);
+        }
+
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            var method = new HttpMethod(Patch);
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = content
+            };
+
+            return await client.SendAsync(request, cancellationToken);
+        }
+
+        public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            var method = new HttpMethod(Patch);
+            var request = new HttpRequestMessage(method, requestUri)
+            {
+                Content = content
+            };
+
+            return await client.SendAsync(request, cancellationToken);
+        }
+
+    }
+}
