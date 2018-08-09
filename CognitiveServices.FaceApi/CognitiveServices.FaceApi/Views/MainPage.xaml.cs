43d06402c0176b2bd6db1a9e0ae509f90d11cfef
Quyk Mendonça <osilmar@outlook.com>
2018-08-08 22:22:46 -0300

    Projeto inicial

---
+++ CognitiveServices.FaceApi/CognitiveServices.FaceApi/Views/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace CognitiveServices.FaceApi.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // List of ValueTuple holding the Navigation Tag and the relative Navigation Page 
+        private readonly IList<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+        {
+            ("home", typeof(HomePage)),
+            ("group", typeof(GroupPage)),
+            ("person", typeof(PersonPage)),
+            ("upload", typeof(UploadPage)),
+            ("train", typeof(TrainPage)),
+            ("identify", typeof(IdentifyPage)),
+            ("verify", typeof(VerifyPage))
+        };
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigated += On_Navigated;
+
+            // NavView doesn't load any page by default: you need to specify it
+            NavView_Navigate("home");
+
+            // Add keyboard accelerators for backwards navigation
+            var goBack = new KeyboardAccelerator { Key = VirtualKey.GoBack };
+            goBack.Invoked += BackInvoked;
+            KeyboardAccelerators.Add(goBack);
+
+            // ALT routes here
+            var altLeft = new KeyboardAccelerator
+            {
+                Key = VirtualKey.Left,
+                Modifiers = VirtualKeyModifiers.Menu
+            };
+            altLeft.Invoked += BackInvoked;
+            KeyboardAccelerators.Add(altLeft);
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            // Getting the Tag from Content (args.InvokedItem is the content of NavigationViewItem)
+            var navItemTag = NavView.MenuItems
+                .OfType<NavigationViewItem>()
+                .First(i => args.InvokedItem.Equals(i.Content))
+                .Tag.ToString();
+
+            NavView_Navigate(navItemTag);
+        }
+
+        private void NavView_Navigate(string navItemTag)
+        {
+            var item = _pages.First(p => p.Tag.Equals(navItemTag));
+            ContentFrame.Navigate(item.Page);
+        }
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            On_BackRequested();
+        }
+
+        private void BackInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
+        {
+            On_BackRequested();
+            args.Handled = true;
+        }
+
+        private bool On_BackRequested()
+        {
+            if (!ContentFrame.CanGoBack)
+            {
+                return false;
+            }
+
+            if (NavView.IsPaneOpen && (NavView.DisplayMode == NavigationViewDisplayMode.Compact || NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+            {
+                return false;
+            }
+
+            ContentFrame.GoBack();
+            return true;
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+            var item = _pages.First(p => p.Page == e.SourcePageType);
+
+            NavView.SelectedItem = NavView.MenuItems
+                .OfType<NavigationViewItem>()
+                .First(n => n.Tag.Equals(item.Tag));
+        }
+    }
+}
